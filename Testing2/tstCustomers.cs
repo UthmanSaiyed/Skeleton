70c01dc91dc60abee20ef70c592e84307211e3bc
Macharia <mmaina374@gmail.com>
2024-05-10 10:49:37 +0100

    Adding testing for customers class

---
+++ Testing2/tstCustomers.cs
@@ -0,0 +1,19 @@
+﻿using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Testing2
+{
+    [TestClass]
+    public class tstCustomers
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            //create an instance of the class we want to create
+            clsCustomers ACustomer = new clsCustomers();
+            //test to see that it exists
+            Assert.IsNotNull(ACustomer);
+        }
+    }
+}
