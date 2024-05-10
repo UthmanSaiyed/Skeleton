90643d5fed7a89e36140d27704b87705bb8ba694
Parth Jayesh <P2591766@my365.dmu.ac.uk>
2024-05-10 10:25:01 +0100

    Adding tests for events class

---
+++ Testing3/tstEvents.cs
@@ -0,0 +1,20 @@
+﻿using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Testing3
+{
+    [TestClass]
+    public class tstEvents
+    {
+        [TestMethod]
+        public void TestEvents()
+        {
+            //create an instance of the class
+            clsEvents AnEvent = new clsEvents();
+
+            //test to see if the instance exist or not
+            Assert.IsNotNull(AnEvent);
+        }
+    }
+}
