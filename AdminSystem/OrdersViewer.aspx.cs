e616920e1ffdefb9981040afaa56faed8f6d148d
Uthman Saiyed <P2722214@my365.dmu.ac.uk>
2024-05-11 18:00:21 +0100

    Updated and added the order classes

--- AdminSystem/OrdersViewer.aspx.cs
+++ AdminSystem/OrdersViewer.aspx.cs
@@ -4,11 +4,40 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using ClassLibrary;
 
 public partial class _1Viewer : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
+        // Check if the session contains the order object and retrieve it
+        if (Session["AnOrder"] != null)
+        {
+            // Cast the session object back to clsOrders type
+            clsOrders AnOrder = (clsOrders)Session["AnOrder"];
 
+            // Display the Ticket ID for this entry
+            Response.Write("Ticket ID: " + AnOrder.TicketID + "<br/>");
+
+            // Display the Customer ID
+            Response.Write("Customer ID: " + AnOrder.CustomerID + "<br/>");
+
+            // Display the Order Status
+            Response.Write("Order Status: " + AnOrder.OrderStatus + "<br/>");
+
+            // Display the Order Date
+            Response.Write("Order Date: " + AnOrder.OrderDate.ToString("dd/MM/yyyy") + "<br/>");
+
+            // Display the Is Paid status
+            Response.Write("Is Paid: " + (AnOrder.IsPaid ? "Yes" : "No") + "<br/>");
+
+            // Display the Total Amount
+            Response.Write("Total Amount: £" + AnOrder.TotalAmount.ToString("N2") + "<br/>");
+        }
+        else
+        {
+            // If the session does not contain the order, display a message
+            Response.Write("No order information available.");
+        }
     }
 }
