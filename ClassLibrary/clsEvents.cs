5bd6f3dac2b59ad2682a6a02e6e8b4eabbddfcf0
Parth Jayesh <P2591766@my365.dmu.ac.uk>
2024-05-13 12:57:58 +0100

    lab 7 is done!!!! All Test Passed!

---
+++ ClassLibrary/clsEvents.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace ClassLibrary
+{
+    public class clsEvents
+    {
+        public bool Active { get; set; }
+        public DateTime DateAdded { get; set; }
+        public int EventID { get; set; }
+        public string Time { get; set; }
+        public string Location { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+
+        // Method to set Time property
+        public void SetTime(string time)
+        {
+            Time = time;
+        }
+
+        // Method to get Time property
+        public string GetTime()
+        {
+            return Time;
+        }
+
+        // Method to set Location property
+        public void SetLocation(string location)
+        {
+            Location = location;
+        }
+
+        // Method to get Location property
+        public string GetLocation()
+        {
+            return Location;
+        }
+
+        // Method to set Title property
+        public void SetTitle(string title)
+        {
+            Title = title;
+        }
+
+        // Method to get Title property
+        public string GetTitle()
+        {
+            return Title;
+        }
+
+        // Method to set Description property
+        public void SetDescription(string description)
+        {
+            Description = description;
+        }
+
+        // Method to get Description property
+        public string GetDescription()
+        {
+            return Description;
+        }
+    }
+}
--- Testing3/tstEvents.cs
+++ Testing3/tstEvents.cs
@@ -16,5 +16,107 @@ namespace Testing3
             //test to see if the instance exist or not
             Assert.IsNotNull(AnEvent);
         }
+
+        [TestMethod]
+        public void TestEventIDProperty()
+        {
+            //creating an instance of the class
+            clsEvents AnEvent = new clsEvents();
+
+            //Creating Test Data 
+            int TestData = 1;
+
+            //Assigning test data
+            AnEvent.EventID = TestData;
+
+            // checking if the data is working
+            Assert.AreEqual(AnEvent.EventID, TestData);
+        }
+
+        [TestMethod]
+        public void TestEventTimeProperty()
+        {
+            // Arrange
+            clsEvents AnEvent = new clsEvents();
+            string testData = "10:00"; // Example test data
+
+            // Act
+            AnEvent.Time = testData;
+
+            // Assert
+            Assert.AreEqual(AnEvent.Time, testData);
+        }
+
+        [TestMethod]
+        public void TestEventLocationProperty()
+        {
+            // Arrange
+            clsEvents AnEvent = new clsEvents();
+            string testData = "Conference Room"; // Example test data
+
+            // Act
+            AnEvent.Location = testData;
+
+            // Assert
+            Assert.AreEqual(AnEvent.Location, testData);
+        }
+
+        [TestMethod]
+        public void TestDateProperty()
+        {
+            // Arrange
+            clsEvents AnEvent = new clsEvents();
+            DateTime testData = DateTime.Now.Date; // Example test data
+
+            // Act
+            AnEvent.DateAdded = testData;
+
+            // Assert
+            Assert.AreEqual(AnEvent.DateAdded, testData);
+        }
+
+        [TestMethod]
+        public void TestEventTitleProperty()
+        {
+            // Arrange
+            clsEvents AnEvent = new clsEvents();
+            string testData = "Event Title"; // Example test data
+
+            // Act
+            AnEvent.Title = testData;
+
+            // Assert
+            Assert.AreEqual(AnEvent.Title, testData);
+        }
+
+        [TestMethod]
+        public void TestEventDescriptionProperty()
+        {
+            // Arrange
+            clsEvents AnEvent = new clsEvents();
+            string testData = "Event Description"; // Example test data
+
+            // Act
+            AnEvent.Description = testData;
+
+            // Assert
+            Assert.AreEqual(AnEvent.Description, testData);
+        }
+
+        [TestMethod]
+        public void TestActiveProperties()
+        {
+            // create an instance of the class
+            clsEvents AnEvent = new clsEvents();
+
+            //creating some test data to assign to the property
+            bool TestData = true;
+
+            //assign a value to test the boolean
+            AnEvent.Active = TestData;
+
+            //If the Active property behaves correctly, this assertion should pass
+            Assert.AreEqual(AnEvent.Active, TestData);
+        }
     }
 }
