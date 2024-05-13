1720dbaec5bd7c6dbc42a8d83c15c46194878eb1
ammaa <ammaa@LAPTOP-THCBNCKG>
2024-05-13 22:08:49 +0100

    lab 8 completed

--- AdminSystem/StockViewer.aspx.cs
+++ AdminSystem/StockViewer.aspx.cs
@@ -11,45 +11,22 @@ public partial class _1Viewer : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-        // Check if the session contains the Stock object and retrieve it
-        
-            // Cast the session object back to clstype
-            clsStock AStock = new clsStock();
-            //get the data from the session object
-            AStock = (clsStock)Session["AStock"];
-            // Display the Ticket ID for this entry
+        if (Session["AStock"] != null)
+        {
+            clsStock AStock = (clsStock)Session["AStock"];
+
             Response.Write("Ticket ID: " + AStock.TicketId + "<br/>");
-
-            //get the data from the session object
-            AStock = (clsStock)Session["AStock"];
-            // Display the Event ID
             Response.Write("Event ID: " + AStock.EventId + "<br/>");
-
-            //get the data from the session object
-            AStock = (clsStock)Session["AStock"];
-            // Display the Quantity
             Response.Write("Quantity: " + AStock.Quantity + "<br/>");
-
-            //get the data from the session object
-            AStock = (clsStock)Session["AStock"];
-            // Display the price
             Response.Write("Price: " + AStock.Price + "<br/>");
-
-            //get the data from the session object
-            AStock = (clsStock)Session["AStock"];
-            // Display the supplier
             Response.Write("Supplier: " + AStock.Supplier + "<br/>");
-
-            //get the data from the session object
-            AStock = (clsStock)Session["AStock"];
-            // Display the Ticket Name
             Response.Write("Ticket Name: " + AStock.TicketName + "<br/>");
-
-            //get the data from the session object
-            AStock = (clsStock)Session["AStock"];
-            // Display what is in Stock
             Response.Write("InStock: " + AStock.InStock + "<br/>");
         }
-  
+        else
+        {
+            Response.Write("Stock information not found in session.");
+        }
     }
+
 }
