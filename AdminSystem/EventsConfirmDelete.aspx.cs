47e0513c9ed30ef3fed239d000d9c6baad2cd495
Parth Jayesh <P2591766@my365.dmu.ac.uk>
2024-06-06 16:37:24 +0100

    14a and 14b done!!!

--- AdminSystem/EventsConfirmDelete.aspx.cs
+++ AdminSystem/EventsConfirmDelete.aspx.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ClassLibrary;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -7,8 +8,28 @@ using System.Web.UI.WebControls;
 
 public partial class _1_ConfirmDelete : System.Web.UI.Page
 {
+    Int32 EventID;
     protected void Page_Load(object sender, EventArgs e)
     {
+        EventID = Convert.ToInt32(Session["EventID"]);
+    }
+
+    protected void btnYes_Click(object sender, EventArgs e)
+    {
+        //create a new instance of the events collection class
+        clsEventsCollection Events = new clsEventsCollection();
+        //find the record to delete
+        Events.ThisEvent.Find(EventID);
+        //delete the record
+        Events.Delete();
+        //Redirect back to the main page
+        Response.Redirect("EventsList.aspx");
+    }
+
+    protected void btnNo_Click(object sender, EventArgs e)
+    {
+        //redirect back to the main page
+        Response.Redirect("EventsList.aspx");
 
     }
 }
